711e7306faea5c72cb9e9744899410346af3e4d6
iamunr4v31 <ashwins1211@gmail.com>
2021-08-16 07:39:04 +0530

    use numpy function

--- 3_rotate_matrix/solution.py
+++ 3_rotate_matrix/solution.py
@@ -1,14 +1,5 @@
-def rotate(mat):
-    new_mat = []
-    for curr, i in enumerate(range(len(mat)-1,-1,-1)):
-        temp = [mat[len(mat)-1-j][curr] for j in range(len(mat))]
-        new_mat.append(temp)
-    return new_mat
-
-
-
-
+import numpy as np
 if __name__ == "__main__":
-    mat = [[1,2,3],[4,5,6],[7,8,9]]
-    # mat = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-    print(rotate(mat))
+    mat = np.array([[1,2,3],[4,5,6],[7,8,9]], int)
+    x = int(input("No of rotations (negative integer for clockwise rotations)"))
+    print(np.rot90(mat, x))
