4e71e137d62a51453490691f7a1eb14e87124a85
iamunr4v31 <ashwins1211@gmail.com>
2021-08-17 23:08:36 +0530

    added reverse rotation

--- 4_rotate_circles_of_matrix/solution.py
+++ 4_rotate_circles_of_matrix/solution.py
@@ -6,16 +6,11 @@ def rotate_circle(mat, n, circle_no=1):
         for j in range(start + 1, end+1):
             if i == start or j == end:
                 temp, mat[i][j] = mat[i][j], temp
-                # mat[i][j] = temp
-            # elif j == end:
-            #     temp, mat[i][j] = mat[i][j], temp
     for i in range(end, start-1, -1):
         for j in range(end-1, start-1, -1):
             if i == end or j == start:
                 mat[i][j], temp = temp, mat[i][j]
-            # elif j == start:
-            #     mat[i][j], temp = temp, mat[i][j]
-    print(*[x for x in mat], sep="\n")
+    return mat
         
                 
     
@@ -23,12 +18,23 @@ def rotate_circle(mat, n, circle_no=1):
 
 
 if __name__ == "__main__":
-    mat = [
-        [1,2,3,4],
-        [5,6,7,8],
-        [9,10,11,12],
-        [13,14,15,16]
-    ]
-    print(*[x for x in mat], sep="\n")
-    print()
-    rotate_circle(mat, 4, 1)
+    # mat = [
+    #     [1,2,3,4],
+    #     [5,6,7,8],
+    #     [9,10,11,12],
+    #     [13,14,15,16]
+    # ]
+    base_mul = 4
+    n = int(input())
+    mat = []
+    for i in range(n):
+        mat.append(input().split())
+    rotations = int(input())
+    if rotations < 0:
+        for x in range(base_mul*(n - 1) + rotations):
+            mat = rotate_circle(mat, n, 1)
+    else:
+        for x in range(rotations):
+            mat = rotate_circle(mat, n, 1)
+    for x in range(n):
+        print(*mat[x])
